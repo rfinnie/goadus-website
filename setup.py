36b0f6c4faacc4cce052ded9ab7f0c2eb17c1954
Ryan Finnie <ryan@finnie.org>
2026-05-01 10:34:53 -0700

    Add explicit build-system defs, stub setup.py for setuptools <61.0.0

---
+++ setup.py
@@ -0,0 +1,9 @@
+# SPDX-PackageName: goadus-website
+# SPDX-PackageSupplier: Ryan Finnie <ryan@finnie.org>
+# SPDX-PackageDownloadLocation: https://codeberg.org/finnix/goadus-website
+# SPDX-FileCopyrightText: © 2026 Ryan Finnie <ryan@finnie.org>
+# SPDX-License-Identifier: MPL-2.0
+
+from setuptools import setup
+
+setup()
